cf8df5d68a3e1fc260a5c87e194b8a666d39c722
tvaishnavi11 <tvaishnavi212@gmail.com>
2026-02-27 19:10:06 +0530

    Update login API endpoint to production URL

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://poject-fullstack.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
